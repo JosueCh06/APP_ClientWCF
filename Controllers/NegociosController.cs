5d9c70551c6504db1015019a8806e26a0e960087
JosuéCh06 <josuechupica06@gmail.com>
2022-11-12 20:02:26 -0500

    first commit

---
+++ Controllers/NegociosController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using APP_ClientWCF.ReferenciasNegocios;
+
+namespace APP_ClientWCF.Controllers
+{
+    public class NegociosController : Controller
+    {
+        // GET: Negocios
+        ServicioNegociosClient dbServicio = new ServicioNegociosClient();
+        public ActionResult listarClientes()
+        {
+            return View(dbServicio.Clientes().ToList());
+        }
+
+        public ActionResult listarClientesXNombres(string nombre = null)
+        {
+            if (nombre == null) nombre = string.Empty;
+            ViewBag.nombre = nombre;
+            return View(dbServicio.ClienteXNombre(nombre).ToList());
+        }
+    }
+}
